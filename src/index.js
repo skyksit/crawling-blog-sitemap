0f8da625dac3810c017774a4d0f559e500c65fa8
Study(ByoungChul) <skyksit@gmail.com>
2022-10-19 16:58:21 +0900

    mod index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import Sitemapper from 'sitemapper';
 import fs from 'fs';
 
+const sitemapUrl = 'https://skyksit.com/post-sitemap-2.xml';
+
 (async () => {
   const Google = new Sitemapper({
-    url: 'https://skyksit.com/sitemap.xml',
+    url: sitemapUrl,
     timeout: 15000, // 15 seconds
   });
 
   try {
     const { sites } = await Google.fetch();
     // console.log(sites);
-    fs.writeFileSync('sitemap.txt', sites.join("\n"));
-
+    fs.writeFileSync('sitemap.txt', sites.join('\n'));
   } catch (error) {
     console.log(error);
   }
